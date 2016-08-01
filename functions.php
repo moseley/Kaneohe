422cb066478fc7b9bc7549177f7a1062b0130117
Jeremy Moseley <jeremymoseley@me.com>
2016-08-01 00:03:07 +0000

    implement custom logo

--- functions.php
+++ functions.php
@@ -138,3 +138,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Implement the Custom Logo feature.
+ */
+require get_template_directory() . '/inc/custom-logo.php';
